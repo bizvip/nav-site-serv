467c42eeada78416cf9672a0c2c96b305054add9
acm311 <acm311@acm311.com>
2023-07-16 20:02:43 +0700

    Update StreamLogProcess.php

--- app/Business/Process/StreamLogProcess.php
+++ app/Business/Process/StreamLogProcess.php
@@ -38,8 +38,7 @@ final class StreamLogProcess extends AbstractProcess
     private function process(): void
     {
         $items = $this->redis->xRead([Publish::COUNTER_KEY => '0-0'], 500, -1);
-        Logger::info(count($items[Publish::COUNTER_KEY]));
-        if (count($items[Publish::COUNTER_KEY]) > 0) {
+        if (isset($items[Publish::COUNTER_KEY]) && count($items[Publish::COUNTER_KEY]) > 0) {
             $list = $this->publishService->batchWriteToDb($items[Publish::COUNTER_KEY]);
             foreach ($list as $v) {
                 $this->redis->xDel(Publish::COUNTER_KEY, $v);
